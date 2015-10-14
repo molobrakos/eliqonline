8bcb6571b0cb036ce570b07d3373157d3874b32e
6D66 <6D66@users.noreply.github.com>
2015-10-14 14:16:44 +0200

    - simple cleanup. - datanow createddate returns a datetime object instead of a string

--- Makefile
+++ Makefile
@@ -1,17 +1,15 @@
+help:
+	@echo "- clean"
+	@echo "- pep8"
+	@echo "- lint"
+
 clean:
 	find . -name '*.pyc' -exec rm -f {} \;
 	find . -name '*.pyo' -exec rm -f {} \;
 	find . -name '*~' -exec rm -f {} \;
+
 pep8:
 	flake8 eliqonline
 
 lint:
 	pylint -E eliqonline
-
-test:
-	nosetests
-
-help:
-	@echo "pep8"
-	@echo "lint"
-	@echo "test"
--- eliqonline/api.py
+++ eliqonline/api.py
@@ -1,10 +1,27 @@
 #!/usr/bin/env python
+#
+#  A library for Eliq Online API
+#  Copyright (C) 2015 Magnus F <magnus@fet.nu>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Lesser Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Lesser Public License for more details.
+#
+# You should have received a copy of the GNU Lesser Public License
+# along with this program.  If not, see [http://www.gnu.org/licenses/].
 
 import urllib
 import json
 
 from .datanow import DataNow
 from .data import Data
+from datetime import datetime
 
 
 class API():
@@ -61,5 +78,8 @@ class API():
             eliq_data_now = DataNow()
             eliq_data_now.power = float(jsonData["power"])
             eliq_data_now.channelid = jsonData["channelid"]
-            eliq_data_now.createddate = jsonData["createddate"]
+            eliq_data_now.createddate = datetime.strptime(
+                jsonData["createddate"],
+                "%Y-%m-%dT%H:%M:%S"
+            )
             return eliq_data_now
