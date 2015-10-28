1a3b52690a6b2a8e7f7a70d983870bae8e9e0f42
6D66 <6D66@users.noreply.github.com>
2015-10-28 10:48:28 +0100

    Bumped up the version to 1.0.5

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
 from distutils.core import setup
 setup(
-    name = 'eliqonline',
-    packages = ['eliqonline'],
-    version = '1.0.4',
-    description = 'Eliq Online API Libary',
-    author = 'Magnus F',
-    author_email = 'magnus@fet.nu',
-    url = 'https://github.com/6D66/eliq/',
-    download_url = 'https://github.com/6D66/eliq/tarball/1.0.4',
-    keywords = ['Eliq', 'Eliq Online', 'Eliq Online API'],
-    classifiers = [],
+    name='eliqonline',
+    packages=['eliqonline'],
+    version='1.0.5',
+    description='Eliq Online API Libary',
+    author='Magnus F',
+    author_email='magnus@fet.nu',
+    url='https://github.com/6D66/eliq/',
+    download_url='https://github.com/6D66/eliq/tarball/1.0.5',
+    keywords=['Eliq', 'Eliq Online', 'Eliq Online API'],
+    classifiers=[],
 )
