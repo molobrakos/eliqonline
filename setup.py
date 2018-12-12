af6d6e1d461f8016ab876706427f5d5f393a00e4
Erik Eriksson <molobrakos@users.noreply.github.com>
2018-12-12 18:23:52 +0100

    Simplified version retrieval

--- setup.py
+++ setup.py
@@ -4,26 +4,16 @@ try:
     from setuptools import setup
 except ImportError:
     from distutils.core import setup
-import io
-import os
 import re
 
 
-def read(*names, **kwargs):
-    with io.open(
-        os.path.join(os.path.dirname(__file__), *names),
-        encoding=kwargs.get("encoding", "utf8"),
-    ) as fp:
-        return fp.read()
-
-
-def find_version(*file_paths):
-    version_file = read(*file_paths)
-    version_match = re.search(
-        r"^__version__ = ['\"]([^'\"]*)['\"]", version_file, re.M
-    )
-    if version_match:
-        return version_match.group(1)
+def find_version(path):
+    with open(path) as f:
+        version_match = re.search(
+            r"^__version__ = ['\"]([^'\"]*)['\"]", f.read(), re.M
+        )
+        if version_match:
+            return version_match.group(1)
     raise RuntimeError("Unable to find version string.")
 
 
@@ -31,13 +21,12 @@ def get_download_url(version):
     return "https://github.com/6d66/eliq/tarball/%s" % version
 
 
-version = find_version("eliqonline", "__init__.py")
+version = find_version("./eliqonline.py")
 package_name = "eliqonline"
 
 setup(
     name=package_name,
-    packages=[package_name],
-    version=find_version(package_name, "__init__.py"),
+    version=version,
     description="Eliq Online API Library",
     author="6d66",
     author_email="",
@@ -46,4 +35,7 @@ setup(
     install_requires=list(open("requirements.txt").read().strip().split()),
     keywords=["Eliq", "Eliq Online", "Eliq Online API"],
     classifiers=[],
+    scripts=["eliqonline"],
+    py_modules=["eliqonline"],
+    extras_require={"console": ["docopt"]},
 )
